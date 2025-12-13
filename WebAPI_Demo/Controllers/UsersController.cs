a9a77854caf21f3c8fd66eebb24784fcdc805449
skyjojo99 <skyjojo0001@gmail.com>
2025-12-14 07:03:53 +0800

    Model done

---
+++ WebAPI_Demo/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI_Demo.Models;
+
+namespace WebAPI_Demo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = new User { Id = id, Name = "John Doe" };
+
+            return Ok(user);
+        }
+    }
+}
---
+++ WebAPI_Demo/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI_Demo.Models
+{
+    public class User
+    {
+         public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
